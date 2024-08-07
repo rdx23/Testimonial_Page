26f3101ab4466a61da36dd831ef6fd56ce987428
Rahul Dora <dorarahul2002@gmail.com>
2024-08-07 21:10:32 +0530

    add

---
+++ src/data.js
@@ -0,0 +1,48 @@
+import Rahul from "./images/Rahul.jpg";
+// import Rahul from "./images/Rahul.jpg";
+// import Rahul from "./images/Rahul.jpg";
+// import Rahul from "./images/Rahul.jpg";
+// import Rahul from "./images/Rahul.jpg";
+
+const reviews = [
+  {
+    id: 1,
+    name: "Rahul Dora",
+    job: "Frontend Developer",
+    image: Rahul,
+    text: "As a beginner frontend developer, I am learning and exploring HTML, CSS, JavaScript, and React. I'm excited to dive into building websites and applications, eager to improve my skills and create visually appealing and interactive interfaces. I enjoy the process of turning ideas into code and am passionate about learning more about frontend development to grow in this field..",
+  },
+  {
+    id: 2,
+    name: "Tas sheikh",
+    job: "Backend Developer",
+    image:
+      "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Rohan Prasad",
+    job: "Wordpress developer",
+    image:
+      "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Nupur Bhavsar",
+    job: "Content Strategist",
+    image:
+      "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Saurabh Singh",
+    job: "Data Analyst",
+    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
